322246402741c435c73c35a958ca6e9669066725
Xaxis <william.neeley@gmail.com>
2015-07-23 11:39:37 -0700

    Corrected dist targeting bug

--- gruntfile.js
+++ gruntfile.js
@@ -42,7 +42,7 @@ module.exports = function(grunt) {
           }
         },
         files: {
-          'dist/jquery.devgrid.min.js': ['src/jquery.plugin.js']
+          'dist/jquery.plugin.min.js': ['src/jquery.plugin.js']
         }
       }
     },
